5b24ded79c0679fbf2f7835f7459591d5c368fc0
Man Leung <lepusleung@gmail.com>
2018-06-18 21:33:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,23 @@
 class SongsController < ApplicationController
   def edit
+    @song = Song.find(params[:id])
   end
 
   def index
+    @songs = Song.all
   end
 
   def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def show
